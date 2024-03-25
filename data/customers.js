2c7a31f1d3d307f1d441f074f7692b7740e32e44
Brooke Anderson <brookeashleyanderson88@gmail.com>
2024-03-24 20:50:57 -0600

    first

---
+++ data/customers.js
@@ -0,0 +1,18 @@
+export const customers = [
+    {
+        "name": "John Smith",
+        "review": "I can't believe how great the service was",
+    },
+    {
+        "name": "Jeff Jefferson",
+        "review": "5 Stars will definitely use them in the future",
+    },
+    {
+        "name": "Linda Johnson",
+        "review": "Had a great experience. They really know how to solve all kinds of problems",
+    },
+    {
+        "name": "Gary Parry",
+        "review": "Changed my oil...Nice",
+    },
+]
